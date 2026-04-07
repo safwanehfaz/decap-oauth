4112e79896b1c4139ba0dad737355c4f8ce0fd72
SAFWAN EHFAZ SAAD <208826388+safwanehfaz@users.noreply.github.com>
2026-04-07 19:21:54 +0600

    Refactor OAuthProvider initialization in server.js

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-const OAuthProvider = require('netlify-cms-oauth-provider-node');
-
-const {
-  ORIGINS,
-  OAUTH_CLIENT_ID,
-  OAUTH_CLIENT_SECRET,
-  REDIRECT_URL,
-  PORT
-} = process.env;
-
-if (!ORIGINS || !OAUTH_CLIENT_ID || !OAUTH_CLIENT_SECRET || !REDIRECT_URL) {
-  console.error(
-    "Missing env vars. Required: ORIGINS, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, REDIRECT_URL"
-  );
-  process.exit(1);
-}
-
 const server = new OAuthProvider({
   providers: {
     github: {
@@ -23,7 +6,3 @@ const server = new OAuthProvider({
     },
   },
 }).server;
-
-server.listen(PORT || 3000, () => {
-  console.log("OAuth server listening");
-});
